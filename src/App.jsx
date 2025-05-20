4578c39be1943824ee9314e5e983637ba3c85c61
MohamedInamulHussain <mohamed160802@gmail.com>
2025-05-20 17:59:02 +0530

    Updated skill icons to use local imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,39 @@ import { useState,useEffect,useRef } from 'react'
 import './App.css'
 import { FaDiscord, FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaTelegram } from 'react-icons/fa'
 import emailjs from "emailjs-com";
+import hussain from './assets/hussainS.svg';
+import abtHolder from './assets/aboutHolder3.webp';
+import foodDontionPro from './assets/projectImg/foodDonationImg.png';
+import onecal from './assets/projectImg/onecal.jpg';
+import cafesite from './assets/projectImg/cafe.jpg';
+import fruitsite from './assets/projectImg/fruity.jpg';
+import javaCertificate from './assets/certificates/java.png';
+import pythonCertificate from './assets/certificates/python.jpg';
+import sqlCertificate from './assets/certificates/SQL.jpg';
+import iste from './assets/achievements/iste.png';
+import paper from './assets/achievements/paper.jpg';
+import naanmudhalvan from './assets/achievements/naanmudhalvan.jpg';
+import presentaion from './assets/achievements/presentaion.jpeg';
+import footGradient from './assets/footDesign.svg';
+import java from './assets/icons/java.png';
+import python from './assets/icons/python.png';
+import spring from './assets/icons/spring.png';
+import sql from './assets/icons/sql.png';
+import mongodb from './assets/icons/mongodb.png';
+import react from './assets/icons/react.png';
+import html from './assets/icons/html.png';
+import css from './assets/icons/css.png';
+import js from './assets/icons/js.png';
+import nodejs from './assets/icons/nodejs.png';
+import tailwind from './assets/icons/tailwind.png';
+import bootstrap from './assets/icons/bootstrap.png';
+import github from './assets/icons/github.png';
+import figma from './assets/icons/figma.png';
+import xd from './assets/icons/xd.png';
+import ae from './assets/icons/ae.png';
+import pr from './assets/icons/pr.png';
+import ps from './assets/icons/ps.png';
+
 function App() {
   const [isDark, setIsDark] = useState(true);
   const handleDownload = () => {
@@ -87,16 +120,35 @@ function App() {
     );
   };
   const achievements = [
-    { title: "ISTE Best Student Award 2024", date: "", description: "", image: "./src/assets/aboutHodler2.webp" },
-    { title: "Received Seed Grant Rs.10,000 Niral Thiruvizha - Hackathon", date: "", description: "", image: "opensource.jpg" },
-    { title: "1st Prize in Technical Quiz", date: "March 2024", description: "Delivered a talk on modern web development trends.", image: "conference.jpg" },
-    { title: "AI Research Paper", date: "April 2024", description: "Published a research paper on AI advancements.", image: "ai_research.jpg" },
-    { title: "Best UI/UX Design", date: "May 2024", description: "Received an award for best UI/UX design in a competition.", image: "uiux.jpg" },
-    { title: "Startup Incubator", date: "June 2024", description: "Selected for a startup incubation program.", image: "startup.jpg" },
-    { title: "Community Mentor", date: "July 2024", description: "Mentored students in coding bootcamps.", image: "mentor.jpg" },
-    { title: "Certified Cloud Engineer", date: "August 2024", description: "Achieved cloud engineering certification.", image: "cloud.jpg" }
+    { title: "ISTE Best Student Award 2024", date: "", description: "", image: iste },
+    { title: "Received Seed Grant Rs.10,000 Niral Thiruvizha - Hackathon", date: "", description: "", image: naanmudhalvan },
+    { title: "Entrepreneurship Cell Leader", date:"", description: "", image: presentaion },
+    { title: "Machine Learning Based Project Paper Published in IEEE", date: "", description: "", image: paper },
+    // { title: "Best UI/UX Design", date: "May 2024", description: "Received an award for best UI/UX design in a competition.", image: presentaion },
+    // { title: "Startup Incubator", date: "June 2024", description: "Selected for a startup incubation program.", image: "startup.jpg" },
+    // { title: "Community Mentor", date: "July 2024", description: "Mentored students in coding bootcamps.", image: "mentor.jpg" },
+    // { title: "Certified Cloud Engineer", date: "August 2024", description: "Achieved cloud engineering certification.", image: "cloud.jpg" }
   ];
-  
+ const skillsSet = [
+  { name: "Java", icon: java },
+  { name: "Python", icon: python },
+  { name: "SpringBoot", icon: spring },
+  { name: "SQL", icon: sql },
+  { name: "MongoDb", icon: mongodb },
+  { name: "React.js", icon: react },
+  { name: "HTML", icon: html },
+  { name: "CSS", icon: css },
+  { name: "JavaScript", icon: js },
+  { name: "Node.js", icon: nodejs },
+  { name: "Tailwind", icon: tailwind },
+  { name: "Bootstrap", icon: bootstrap },
+  { name: "GitHub", icon: github },
+  { name: "Figma", icon: figma },
+  { name: "AdobeXD", icon: xd },
+  { name: "AfterEffects", icon: ae },
+  { name: "PremierPro", icon: pr },
+  { name: "Photoshop", icon: ps },
+];
 
   return (
     <div className={`app ${isDark ? 'dark' : 'light'}`}>
@@ -119,11 +171,10 @@ function App() {
           <div className="hero-content">
             <h2>FULL-STACK WEB DEVELOPER</h2>
             <h1>Mohamed Inamul<br />Hussain</h1>
-            <p>I'm a full-stack web developer and I work remotely from South India.</p>
-           
+            <p>I'm a full-stack web developer and I work remotely from South India.</p>          
           </div>
           <div className="hero-image">
-            <img src=".\src\assets\hussainS.svg" alt="Developer" />      
+            <img src={hussain} alt="Developer" />      
             <div className='cv-img' onClick={handleDownload} 
                         style={{ cursor: "pointer"}}></div>    
           </div>
@@ -152,7 +203,7 @@ function App() {
       
         <section className="about">       
           <div className="about-image">
-            <img src=".\src\assets\aboutHolder3.webp" alt="About" />
+            <img src={abtHolder} alt="About" />
           </div>
           <div className="about-content">
             <h2>About<br /> me</h2>
@@ -166,44 +217,52 @@ function App() {
         <section className="skills" id="skills">
       <h2>My Skills</h2>
       <div className="skills-container">
-        {skills.map((skill, index) => (
-          <div key={index} className="skill-card">
-            <img src={skill.icon} alt={skill.name} className="skill-icon" />
-            <p>{skill.name}</p>
-          </div>
-        ))}
-      </div>
+            {skillsSet.map((skill, index) => (
+        <div key={index} className="skill-card">
+          <img src={skill.icon} alt={skill.name} className="skill-icon" />
+          <p>{skill.name}</p>
+        </div>
+           ))}
+    </div>
     </section>
 
         <section className="projects" id="projects">
           <h2>My projects</h2>
           <div className="project-grid">
             <div className="project-card">
-              <img src=".\src\assets\projectImg\foodDonationImg.png" alt="Project 1" />
-              <div className="project-info">
-                <h3>Food Donation Application</h3>
-                <span>June 2024</span>
-              </div>
-            </div>
-            <div className="project-card">
-              <img src=".\src\assets\projectImg\onecal.jpg" alt="Project 2" />
+              <img src={onecal} alt="Project 2" />
               <div className="project-info">
                 <h3>OneCal - Meet Scheduler</h3>
                 <span>JAN 2025</span>
+                <br></br>
+                <a href='https://onecal-scheduler-deploy-1.onrender.com/' style={{color:"#E91E63"}}>Click here..</a>
               </div>
             </div>
             <div className="project-card">
-              <img src=".\src\assets\projectImg\cafe.jpg" alt="Project 3" />
+              <img src={cafesite} alt="Project 3" />
               <div className="project-info">
                 <h3>Wildbean Cafe Website</h3>
                 <span>Feb 2025</span>
+                 <br></br>
+                <a href='https://mohamedinamulhussain.github.io/Wild_bean_Cafe-Website/' style={{color:"#E91E63"}}>Click here..</a>
               </div>
             </div>
             <div className="project-card">
-              <img src=".\src\assets\projectImg\fruity.jpg" alt="Project 4" />
+              <img src={fruitsite} alt="Project 4" />
               <div className="project-info">
                 <h3>Fruity Web UI Design</h3>
                 <span>Mar 2025</span>
+                 <br></br>
+                <a href='https://mohamedinamulhussain.github.io/Fruity-React-Project/' style={{color:"#E91E63"}}>Click here..</a>
+              </div>
+            </div>
+            <div className="project-card">
+              <img src={foodDontionPro} alt="Project 1" />
+              <div className="project-info">
+                <h3>Food Donation Application</h3>
+                <span>June 2024</span>
+                 <br></br>
+                <a href='' style={{color:"#E91E63"}}>Under Deployment..</a>
               </div>
             </div>
             <a href="#" className="see-more">See more projects →</a>
@@ -214,7 +273,7 @@ function App() {
           <h2>Certification</h2>
           <div className="blog-grid">
             <div className="blog-card">
-              <img src="src/assets/certificates/java.png" alt="java certificate" />
+              <img src={javaCertificate} alt="java certificate" />
               <div className="blog-content">
                 <h3>Java Full stack Certification</h3>
                 <p style={{marginBottom:"1rem"}}>Wipro pvt Ltd</p>
@@ -223,7 +282,7 @@ function App() {
               </div>
             </div>
             <div className="blog-card">
-              <img src="src/assets/certificates/python.jpg" alt="python certificate" />
+              <img src={pythonCertificate} alt="python certificate" />
               <div className="blog-content">
                 <h3>Pyhton Certification</h3>
                 <p>Cognitive Clases powered by IBM Developer skill network</p>
@@ -231,7 +290,7 @@ function App() {
               </div>
             </div>
             <div className="blog-card">
-              <img src="src/assets/certificates/SQL.jpg" alt="SQL certificate" />
+              <img src={sqlCertificate} alt="SQL certificate" />
               <div className="blog-content">
                 <h3>SQL and Relational Database</h3>
                 <p>Cognitive Clases powered by IBM Developer skill network</p>
@@ -260,12 +319,13 @@ function App() {
           </div>
         ))}
       </div>
+      <a href="#" className="see-more">See more projects →</a>
     </div>
         </section>
 
         <section className="contact" id="contact">
           <h2>Contact me</h2>
-          <div className='footGradient'><img src='src/assets/footDesign.svg' className='footDesign'/></div>
+          <div className='footGradient'><img src={footGradient} className='footDesign'/></div>
           <form ref={form} onSubmit={sendEmail} className="contact-form">
       <div className="form-group">
         <input type="text" name="name" placeholder="Full name*" required />
